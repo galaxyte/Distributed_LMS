711cf0337260b1ed1fff3479effdd558b36a97ab
Parth Tiwari <parthjtgjs851@gmail.com>
2025-04-09 12:19:38 +0530

    Update Footer.jsx

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ Parth Tiwari © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Parth Tiwari and Utkarsh Vinayak © 2023 Studynotion</div>
         </div>
       </div>
     </div>
